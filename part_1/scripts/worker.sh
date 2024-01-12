844e125d830142a250e6cfbc00b5f411917eb1bb
Mathia PAGANI <mathia.pagani@cc.in2p3.fr>
2024-01-12 14:18:18 +0100

    k3s agent node well reaching out server and kubectl get nodes well listing server + worker nodes

--- part_1/scripts/worker.sh
+++ part_1/scripts/worker.sh
@@ -3,10 +3,16 @@
 export NAME="mpaganiSW"
 
 export TOKEN_FILE="/vagrant/scripts/node-token"
+export NEW_TOKEN_FILE="/etc/rancher/k3s/node-token"
 
-export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file $TOKEN_FILE --node-ip=$2"
+# Create the new token file and write the token from the old file to it
+sudo mkdir -p /etc/rancher/k3s && sudo sh -c "echo $(cat $TOKEN_FILE) > $NEW_TOKEN_FILE"
 
-apk add curl
+export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file $NEW_TOKEN_FILE --node-ip=$2"
+
+if ! command -v curl &> /dev/null; then
+  apk add curl
+fi
 
 curl -sfL https://get.k3s.io | sh -
 

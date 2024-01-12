34aca0608d5bddb3beba363eed19565239d61edf
Mathia PAGANI <mathia.pagani@cc.in2p3.fr>
2024-01-12 14:18:18 +0100

    k3s agent node well reaching out server and kubectl get nodes well listing server + worker nodes

--- part_1/scripts/server.sh
+++ part_1/scripts/server.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+export NAME="mpaganiS"
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1 "
 
-apk add curl
+if ! command -v curl &> /dev/null; then
+   apk add curl
+fi
 
 curl -sfL https://get.k3s.io |  sh -
 
-sleep 10
+# Wait for K3s to be ready
+while ! rc-service k3s status &>/dev/null; do
+  sleep 2
+done
+
+# Wait for the node-token file to be created
+while [[ ! -f /var/lib/rancher/k3s/server/node-token ]]; do
+ sleep 2
+done
 
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/scripts/
 
-echo "[INFO]  Successfully installed k3s on server node"
+echo "[WE DID IT FRANK !]  Successfully installed k3s on server node"
 
 echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
 

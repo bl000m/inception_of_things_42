2053b8747bf1ee376dbc89a0198d6d2f407d7be7
Mathia PAGANI <mathia.pagani@cc.in2p3.fr>
2024-01-13 21:35:17 +0100

    adding setup script

---
+++ part_3/setting/install_dependencies.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# installind Docker
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# install Docker latest version
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#install K3D
+wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+
+# install kubectl
+sudo snap install kubectl --classic
